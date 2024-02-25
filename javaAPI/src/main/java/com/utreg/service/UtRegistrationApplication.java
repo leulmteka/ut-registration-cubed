c3390bc88142419d242bf3c2ad138a7efdbf7e08
Leul Teka <leulteka@Leuls-Air.lan>
2024-02-24 20:57:47 -0600

    initial commit

---
+++ javaAPI/src/main/java/com/utreg/service/UtRegistrationApplication.java
@@ -0,0 +1,13 @@
+package com.utreg.utregistration;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UtRegistrationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UtRegistrationApplication.class, args);
+	}
+
+}
